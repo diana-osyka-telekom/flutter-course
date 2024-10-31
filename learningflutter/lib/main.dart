9a97a20cb2cdfe17b0dcdcbc591aea2471ce3a09
diana-osyka-telekom <diana.osyka@telekom.com>
2024-10-31 14:37:01 +0100

    Steps 5

--- learningflutter/lib/main.dart
+++ learningflutter/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learningflutter/views/login_view.dart';
 import 'package:learningflutter/views/register_view.dart';
+import 'constants/routes.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -13,8 +14,8 @@ void main() {
     ),
     home: const HomePage(),
     routes: {
-      '/login': (context) => const LoginView(),
-      '/register' : (context) => const RegisterView()
+      loginRoute: (context) => const LoginView(),
+      registerRoute : (context) => const RegisterView()
     }
   ));
 }
